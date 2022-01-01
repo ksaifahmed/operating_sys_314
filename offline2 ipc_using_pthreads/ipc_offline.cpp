290f7105ae23376d56f746fceb925e6e4af79ef5
ksaifahmed <ksaifahmed@gmail.com>
2022-01-01 19:05:46 +0600

    offline 2 -> kiosks init

---
+++ offline2 ipc_using_pthreads/ipc_offline.cpp
@@ -0,0 +1,114 @@
+#include<cstdio>
+#include<pthread.h>
+#include<semaphore.h>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+//semaphores
+sem_t kiosk_empty;
+sem_t kiosk_full;
+sem_t kiosk_mutex;
+
+//constants to be replaced by file i/o
+#define M 6
+#define POISSON 1
+int gid_count = 0;
+
+
+class Passenger{
+    bool vip;
+    int id;
+
+    public:
+        Passenger(bool, int);
+        bool is_vip();
+        int getID();
+};
+
+Passenger::Passenger(bool v, int i)
+{
+    vip = v;
+    id = i;
+}
+
+bool Passenger::is_vip()
+{
+    return vip;
+}
+
+int Passenger::getID(){ return id; }
+
+
+//Waiting Lines
+queue<Passenger> kiosk_line;
+queue<Passenger> kiosks;
+
+void * SentToAirport(void * arg)
+{
+    Passenger pass(((Passenger *)arg)->is_vip(), ((Passenger *)arg)->getID());
+    cout << "Passenger " << pass.getID() <<" arrived at airport!" << endl;
+    sem_wait(&kiosk_empty);
+    sem_wait(&kiosk_mutex);
+
+    //push into kiosks here
+    kiosks.push(pass);
+
+    sem_post(&kiosk_mutex);
+    sem_post(&kiosk_full);
+}
+
+
+void * PassengerProducer(void * arg)
+{
+    while(1)
+    {
+        sleep(POISSON);
+        pthread_t temp;
+        Passenger * pass = new Passenger(rand()%2, ++gid_count);
+        pthread_create(&temp, NULL, SentToAirport, pass);
+        cout << "new passenger with id:" << pass -> getID() << ", status: "<< pass -> is_vip() << endl;
+    }
+}
+
+
+
+void * KioskHandler(void * arg)
+{
+    while(1)
+    {
+        sem_wait(&kiosk_full);
+        sem_wait(&kiosk_mutex);
+
+        //pop from kiosks to push into random belt
+        sleep(2);
+        Passenger p = kiosks.front();
+        kiosks.pop();
+        cout << "Passenger " << p.getID() << " done at kiosk!\n";
+
+        sem_post(&kiosk_mutex);
+        sem_post(&kiosk_empty);
+    }
+}
+
+
+
+int main()
+{
+    srand(time(NULL));
+    pthread_t pass_gen_thread;
+    pthread_t kiosk_thread;
+
+    //init_semaphore();
+    sem_init(&kiosk_empty, 0, M);
+    sem_init(&kiosk_full, 0, 0);
+    sem_init(&kiosk_mutex, 0, 1);
+
+	char * dummy_msg = "";
+	pthread_create(&pass_gen_thread, NULL, PassengerProducer, (void*)dummy_msg);
+	pthread_create(&kiosk_thread, NULL, KioskHandler, (void*)dummy_msg);
+
+	pthread_exit(NULL);
+    return 0;
+}
+
