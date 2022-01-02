c5ad667d243c9826340ad08ee71f3abecc845207
ksaifahmed <ksaifahmed@gmail.com>
2022-01-02 21:09:54 +0600

    offline 2 -> boarding init

--- offline2 ipc_using_pthreads/ipc_offline.cpp
+++ offline2 ipc_using_pthreads/ipc_offline.cpp
@@ -13,6 +13,8 @@ sem_t kiosk_full;
 sem_t bin_sem;
 sem_t * belt_sem_full;
 sem_t * belt_sem_emp;
+sem_t boarding_empty;
+sem_t boarding_full;
 
 //constants to be replaced by file i/o
 #define POISSON 1
@@ -29,27 +31,31 @@ int get_sys_time()
 }
 
 class Passenger{
-    bool vip;
-    int id, s_time = 0;
+    int id, belt = -1;
+    bool vip, pass = true;
 
     public:
         Passenger(bool v, int i){vip = v; id = i;}
         bool is_vip(){ return vip; }
+        bool has_pass(){ return pass; }
         int getID(){ return id; }
-        int get_stime(){ return s_time; }
-        void inc_stime(int t){ s_time += t; }
+        int getBelt(){ return belt; }
+        void setBelt(int b){ belt = b; }
+        void set_pass(bool b){ pass = b; }
+
 };
 
 
 //Waiting Lines
-queue<Passenger> kiosks;
-queue<Passenger> * belts;
+queue<Passenger*> kiosks;
+queue<Passenger*> * belts;
+queue<Passenger*> board_officer;
 
 void * SentToAirport(void * arg)
 {
-    Passenger pass(((Passenger *)arg)->is_vip(), ((Passenger *)arg)->getID());
+    Passenger * pass = (Passenger *) arg;
     sem_wait(&bin_sem);
-    cout << "Passenger " << pass.getID() <<" has arrived at airport at time " << get_sys_time() << endl;
+    cout << "Passenger " << pass->getID() <<" has arrived at airport at time " << get_sys_time() << endl;
     sem_post(&bin_sem);
 
     sem_wait(&kiosk_empty);
@@ -76,7 +82,7 @@ void * PassengerProducer(void * arg)
 }
 
 
-
+void * SendToSecurity(void * arg);
 void * KioskHandler(void * arg)
 {
     int kiosk_id = 0;
@@ -90,30 +96,40 @@ void * KioskHandler(void * arg)
         sem_wait(&kiosk_full);
         sem_wait(&bin_sem);
 
-        Passenger p = kiosks.front();
-        cout << "Passenger " << p.getID() << " has started self-check in at kiosk " << kiosk_id << " at time " << get_sys_time() << endl;
+        Passenger * p = kiosks.front();
+        cout << "Passenger " << p->getID() << " has started self-check in at kiosk " << kiosk_id << " at time " << get_sys_time() << endl;
         sleep(w);
         kiosks.pop();
-        cout << "Passenger " << p.getID() << " has finished check in at time " << get_sys_time() << endl;
+        cout << "Passenger " << p->getID() << " has finished check in at time " << get_sys_time() << endl;
 
         sem_post(&bin_sem);
         sem_post(&kiosk_empty);
 
 
         //push into random belt
-        sem_wait(&belt_sem_emp[belt_no]);
-        sem_wait(&bin_sem);
-
-        belts[belt_no].push(p);
-        cout << "Passenger " << p.getID() << " has started waiting for security check in belt " << belt_no << " at time " << get_sys_time() << endl;
-
-        sem_post(&bin_sem);
-        sem_post(&belt_sem_full[belt_no]);
-
+        pthread_t temp;
+        p->setBelt(belt_no);
+        pthread_create(&temp, NULL, SendToSecurity, p);
     }
 }
 
 
+void * SendToSecurity(void * arg)
+{
+    Passenger * p = (Passenger *) arg;
+
+    sem_wait(&belt_sem_emp[p->getBelt()]);
+    sem_wait(&bin_sem);
+
+    belts[p->getBelt()].push(p);
+    cout << "Passenger " << p->getID() << " has started waiting for security check in belt " << p->getBelt() << " at time " << get_sys_time() << endl;
+
+    sem_post(&bin_sem);
+    sem_post(&belt_sem_full[p->getBelt()]);
+
+}
+
+void * SentToBoarding(void * arg);
 void * SecurityCheck(void * arg)
 {
     while(1)
@@ -122,17 +138,59 @@ void * SecurityCheck(void * arg)
         sem_wait(&belt_sem_full[*belt_id]);
         sem_wait(&bin_sem);
 
-        Passenger p = belts[*belt_id].front();
-        cout << "Passenger " << p.getID() << " has started the security check at time " << get_sys_time() << endl;
+        Passenger * p = belts[*belt_id].front();
+        cout << "Passenger " << p->getID() << " has started the security check at time " << get_sys_time() << endl;
         sleep(x);
         belts[*belt_id].pop();
-        cout << "Passenger " << p.getID() << " has crossed the security check at time " << get_sys_time() << endl;
+        cout << "Passenger " << p->getID() << " has crossed the security check at time " << get_sys_time() << endl;
 
         sem_post(&bin_sem);
         sem_post(&belt_sem_emp[*belt_id]);
+
+        //send to random boarding
+        pthread_t temp;
+        bool b_pass = rand()%2;
+        p->set_pass(b_pass);
+        pthread_create(&temp, NULL, SentToBoarding, p);
     }
 }
 
+
+void * SentToBoarding(void * arg)
+{
+    Passenger * p = (Passenger *) arg;
+    sem_wait(&bin_sem);
+    cout << "Passenger " << p->getID() <<" has started waiting to be boarded at time " << get_sys_time() << endl;
+    sem_post(&bin_sem);
+
+    sem_wait(&boarding_empty);
+    sem_wait(&bin_sem);
+
+    //gone in to be checked by officer
+    board_officer.push(p);
+
+    sem_post(&bin_sem);
+    sem_post(&boarding_full);
+}
+
+
+void * BoardingHandler(void * arg)
+{
+    while(1)
+    {
+        sem_wait(&boarding_full);
+        sem_wait(&bin_sem);
+
+        Passenger * pass = board_officer.front();
+        cout << "Passenger " << pass->getID() <<" has started boarding the plane at time  " << get_sys_time() << endl;
+        sleep(y);
+        board_officer.pop();
+        cout << "Passenger " << pass->getID() <<" has boarded the plane at time  " << get_sys_time() << endl;
+
+        sem_post(&bin_sem);
+        sem_post(&boarding_empty);
+    }
+}
 
 
 void init_sem()
@@ -150,6 +208,10 @@ void init_sem()
         sem_init(&belt_sem_full[i], 0, 0);
     }
 
+    //boarding sem
+    sem_init(&boarding_empty, 0, 1);
+    sem_init(&boarding_full, 0, 0);
+
 }
 
 void init_pthreads()
@@ -158,6 +220,7 @@ void init_pthreads()
     pthread_t pass_gen_thread;
     pthread_t kiosk_thread;
     pthread_t belt_threads[N];
+    pthread_t boarding_thread;
 
 	pthread_create(&pass_gen_thread, NULL, PassengerProducer, (void*)dummy_msg);
 	pthread_create(&kiosk_thread, NULL, KioskHandler, (void*)dummy_msg);
@@ -168,13 +231,15 @@ void init_pthreads()
         pthread_create(&belt_threads[i], NULL, SecurityCheck, (void *)belt_id);
 	}
 
+	pthread_create(&boarding_thread, NULL, BoardingHandler, (void*)dummy_msg);
+
 }
 
 
 int main()
 {
     srand(time(NULL));
-    belts = new queue<Passenger>[N];
+    belts = new queue<Passenger*>[N];
 
     init_sem();
     init_pthreads();
