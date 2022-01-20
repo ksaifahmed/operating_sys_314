83284271f0b75f6e17ba3e44a8575619414581b6
ksaifahmed <ksaifahmed@gmail.com>
2022-01-21 01:07:16 +0600

    offline3 -> init 2.0

---
+++ offline3_xv6_lottery_scheduler/xv6/launchp.c
@@ -0,0 +1,63 @@
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(int argc, char *argv[])
+{
+	if(argc != 1){
+		printf(1, "Invalid params!\n");
+		exit();
+	}
+
+    int id = 0, i, j;
+    double x = 1.01012;
+    struct pstat *proc_info = (struct pstat*) malloc(sizeof(struct pstat));
+
+    //parent creates first child
+    id = fork();
+    if(id < 0) printf(1, "Failed to fork first child!\n");
+
+    //parent creates second child
+    if(id > 0) {
+        id = fork();
+        if(id < 0) printf(1, "Failed to fork second child!\n");
+    }
+
+    //parent creates third child
+    if(id > 0) {
+        id = fork();
+        if(id < 0) printf(1, "Failed to fork second child!\n");
+    }
+
+    //parent inf loop
+    if(id > 0){
+        while(1)
+        {
+            if(getpinfo(proc_info) == -1){
+                printf(1, "Something went wrong when retrieving process info!");
+                wait();
+            }
+            printf(1, "PID\tTICKS\tTICKETS\n");
+            for(i = 0; i < NPROC; i++){
+                if(proc_info->pid[i] == 1 || proc_info->pid[i] == 2)
+                 continue; //parent and shell process
+                
+                if(proc_info->inuse[i]){
+                    printf(1, "%d\t%d\t%d\n", proc_info->pid[i], proc_info->ticks[i], proc_info->tickets[i]);
+                }
+            }            
+            sleep(100);         
+        }
+    }
+
+    //child does a heavy calculation
+    if(!id){
+        while(1)
+        {
+            for(j = 0; j < 214748000; j++)
+                x += 0.0131*45.445;
+        }
+    }
+
+	exit();
+}
